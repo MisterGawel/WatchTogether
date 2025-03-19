2b89f319b47bf76ca4ef716481854552972e7f89
Gael Amoros <gaelamoros13@gmail.com>
2025-03-19 08:53:47 +0100

    Initialisation du projet avec NextJS et toutes les dépendances

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyD1ykPx6lritVN81q3mC4ayLHywfA_OM9M',
+	authDomain: 'watchtogamer.firebaseapp.com',
+	projectId: 'watchtogamer',
+	storageBucket: 'watchtogamer.firebasestorage.app',
+	messagingSenderId: '664072514206',
+	appId: '1:664072514206:web:2a3a997f2a1722913da98f',
+	measurementId: 'G-3Y4BBRJLMR',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
