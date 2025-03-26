3572d6352fbae2070a24ee4435d39513c883a196
Lucas Rosenzweig <xeit0w@gmail.com>
2025-03-26 11:25:11 +0100

    Implémentation des test + début wrapper

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
     return (
       <div>
             <main className="">
-                <WrapperPlayer Player={YoutubePlayer} videoId={"7jYGNkGZxgY"}/>
+                <WrapperPlayer link={"https://www.twitch.tv/BDECERI"}/>
+                <WrapperPlayer link={"https://www.youtube.com/watch?v=6n3pFFPSlW4"}/>
+                <WrapperPlayer link={"https://www.dailymotion.com/video/x7s7z9d"}/>
             </main>
       </div>
     );
--- app/players/wrapper-player.jsx
+++ app/players/wrapper-player.jsx
@@ -1,10 +1,54 @@
-const WrapperPlayer = ({ Player, videoId }) => {
+import YoutubePlayer from "./youtube-player";
+import DailymotionPlayer from "./dailymotion-player";
+import TwitchPlayer from "./twitch-player";
 
-    return (
-        <div className='relative w-full h-full'>
-            <Player videoId={videoId} channel={videoId} />
-        </div>
-    );
+const WrapperPlayer = ({ link }) => {
+
+    // Get the domain name from the URL
+    const url = new URL(link);
+    const domain = url.hostname.replace('www.', '');
+
+    function getVideoId(url) {
+        //Youtube example : https://www.youtube.com/watch?v=jfKfPfyJRdk
+        if (domain === 'youtube.com') {
+            const searchParams = new URLSearchParams(url.search);
+            return searchParams.get('v');
+        }
+
+        //Dailymotion example : https://www.dailymotion.com/video/x9g8p00
+        if (domain === 'dailymotion.com') {
+            return url.pathname.split('/')[2];
+        }
+
+        //Twitch example : https://www.twitch.tv/sixentv
+        if (domain === 'twitch.tv') {
+            return url.pathname.split('/')[1];
+        }
+    }
+
+    if (domain === 'youtube.com') {
+        return (
+            <YoutubePlayer videoId={getVideoId(url)} />
+        );
+    }
+
+    else if (domain === 'dailymotion.com') {
+        return (
+            <DailymotionPlayer videoId={getVideoId(url)} />
+        );
+    }
+
+    else if (domain === 'twitch.tv') {
+        return (
+            <TwitchPlayer channel={getVideoId(url)} />
+        );
+    }
+
+    else {
+        return (
+            <div>Video not supported</div>
+        );
+    }
 };
 
 export default WrapperPlayer;
